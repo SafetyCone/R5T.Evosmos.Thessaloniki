afc67b30dd4237605c13f310d2669f3c76d504bc
MinexAutomation <minex.automation@gmail.com>
2020-01-30 16:39:47 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Evosmos.Thessaloniki/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+using R5T.Lombardy;
+
+
+namespace R5T.Evosmos.Thessaloniki
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="TemporaryDirectoryFilePathProvider"/> implementation of <see cref="ITemporaryDirectoryFilePathProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddThessalonikiTemporaryDirectoryFilePathProvider(this IServiceCollection services,
+            ServiceAction<ITemporaryDirectoryFilePathProvider> addTemporaryDirectoryFilePathProvider,
+            ServiceAction<IStringlyTypedPathOperator> addStringlyTypedPathOperator)
+        {
+            services
+                .AddSingleton<ITemporaryDirectoryFilePathProvider, TemporaryDirectoryFilePathProvider>()
+                .RunServiceAction(addTemporaryDirectoryFilePathProvider)
+                .RunServiceAction(addStringlyTypedPathOperator)
+                ;
+
+            return services;
+        }
+    }
+}
